f6a79ad198610d24217c02b8b2b118537a46111d
akil20231 <akhilpeteti07@gmail.com>
2026-08-05 13:19:29 -0400

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-0

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def inBetween(self, start, mid, end):
+        return start < mid <= end
+
+
+
+    def specialArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+
+        if nums[0] >= n:
+            return n
+            
+        prev = nums[0]
+
+        for i in range(1, n):
+            curr = nums[i]
+            total = n - i
+
+            if self.inBetween(prev, total, curr):
+                return total
+
+            prev = curr
+
+        return -1
+
